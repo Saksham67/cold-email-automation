b59c1d3fb408fe97a3d0c0f952d0132c4bf8db76
Saksham67 <sakshamkumar652547@gmail.com>
2025-12-11 03:01:56 +0530

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,4 @@ app.use("/send-emails", emailRoutes);
 app.use(express.static(path.join(__dirname, "..", "frontend")));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
